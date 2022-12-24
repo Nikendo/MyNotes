56746306fe069a7755e71def0ce9f179b0dbfb64
Nikita Shmatov <pravdomir42@gmail.com>
2022-12-25 01:14:53 +0200

    Initial Commit

---
+++ MyNotes/MyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNotesApp.swift
+//  MyNotes
+//
+//  Created by Shmatov Nikita on 25.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
